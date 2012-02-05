ee7ec3a69c0103d6c0f53d1bc04e3e87b536c9ae
siserte <sergioiserte@gmail.com>
2012-02-05 11:57:42 +0100

    arreglando el repo

--- src/es/siserte/cuentakm/MainActivity.java
+++ src/es/siserte/cuentakm/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Log.d("estado", "onStop");
+		//Log.d("estado", "onStop");
 	}
 
 	@Override
